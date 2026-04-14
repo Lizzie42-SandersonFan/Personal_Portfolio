6411a92ed1af8f38e740d6d026f0e5ecf33fbef9
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-04-14 09:43:08 -0600

    I finished!!!

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Personal_Portfolio
+
+## How to Use
+Before running the program, you need to install tkinter. Type the folowing command into your terminal:
+- pip install tkinter
+
+After that is complete, go the the file called 'main.py' and hit the run button. A window will pop up with buttons you can press!
+
+## Project Features
+- Tkinter User Interface
+- Buttons to navigate to different windows
+- Buttons to run the featured programs
+
+## Contributors
+- Lizzie42-SandersonFan
--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
 # LD 1st Personal Portfolio
-from main_func import big_main
+from main_func import *
 
 # Call main_func function
-big_main()
+root = tk.Tk()
+# All the stuff
+root.title("Home")
+root.minsize(1000, 1000)
+root.maxsize(2000,2000)
+root.geometry("300x300+100+100")
+
+label = tk.Label(root, text="Welcome to my programming portfolio!\nClick on any of the buttons to learn about a project I have built and get to run it!", font=("Comic Sans", 14, "bold"))
+
+# Buttons
+btn1 = tk.Button(root, text="Movie Recomender", command=movie_window)
+btn1.pack()
+
+btn2 = tk.Button(root, text="Gradebook Manager", command=gradebook_window)
+btn2.pack()
+
+btn3 = tk.Button(root, text="Pet Simulator", command=pet_sim_window)
+btn3.pack()
+
+btn4 = tk.Button(root, text="Sierpinski Triangle Generator", command=triangle_window)
+btn4.pack()
+
+btn5 = tk.Button(root, text="Close Program", command=root.destroy)
+btn5.pack()
+
+label.pack()
+root.mainloop()
